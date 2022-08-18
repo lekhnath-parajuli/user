ba8bc4b1435f9721853079bb5ffebe5b9be6dadc
Lekhnath Parajuli <lekhnathparajuli4@gmail.com>
2022-08-18 22:13:07 +0545

    Add configurations

---
+++ config.py
@@ -0,0 +1,22 @@
+from pydantic import BaseSettings
+
+
+class DBConfig:
+    HOST: str = 'localhost'
+    USER: str = 'uttam'
+    PASSWORD: str = ''
+    NAME: str = 'fastapi'
+    DATABASE: str = 'postgresql'
+
+
+class Config(BaseSettings):
+    HOST: str = 'localhost'
+    PORT: int = 8080
+    DATABASE_URL: str = f'{DBConfig.DATABASE}://{DBConfig.USER}:{DBConfig.PASSWORD}@{DBConfig.HOST}/{DBConfig.NAME}'
+    RABBITMQ_SERVER: str = 'amqp://guest:guest@localhost/'
+    ALLOWED_TOPICS: list = [
+        'user'
+    ]
+
+
+config = Config()
